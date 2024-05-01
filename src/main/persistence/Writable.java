aeb822ee8d81a5404d253d34ff9d0933db263912
CdrePuddles <mr_me66@Hotmail.com>
2024-04-30 22:28:02 -0700

    Early bird submission - Phase 4 Phase 4 Task 3
    Updated the README.md file with Phase 4 Task 3 analysis.
    
    Included a UML diagram labeled UML_Design_Diagram.pdf.
    
    Incorporated EventLog in the following areas:
    - InventoryManagement Constructor (for the purposes of logging the creation of a list or loading)
    - addItem
    - removeItem
    - getItemFromId (search)
    - getList (search)
    - toJson (for the purposes of logging saving)

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -6,5 +6,5 @@ public interface Writable {
     // EFFECTS:     returns this as JSON object
     // CREDIT:      this portion is substantively modelled off of the JsonSerializationDemo
     //              provided as a reference for the term project
-    JSONObject toJson();
+    //JSONObject toJson();
 }
